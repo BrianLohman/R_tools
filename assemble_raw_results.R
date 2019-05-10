9b41844f56c8b2b4b002df94d22d85921fdc7b8e
BrianLohman <brian.keith.lohman@gmail.com>
2019-05-10 15:56:37 -0600

    expanded unpacking individual files, need to finish master list of genes

--- assemble_raw_results.R
+++ assemble_raw_results.R
@@ -6,31 +6,40 @@ filenames <- Sys.glob("*.RData")
 # empty sapce to store master resutls list and iteration ID
 master_results = list()
 iterations = c()
-sample_ids = list()
-genes = list()
+master_samples = list()
+master_genes = list()
+k = -1 # for indexing into lists
+f = 0 # for indexing filenames
 
-# unpack RData objects
-for(i in filenames){
+# unpack RData objects, 100 total each with a unique filename
+for(i in filenames){ 
+    # increment counters
+    f = f + 1
+    k = k + 1
+
     # load file into  memory
     load(i)
 
-    # append to master list
-    master_results = c(master_results, mc_results)
+    # loop through the 100 simulations in each of the files
+    for(j in 1:100){
+        # add file name to list of iterations
+        iterations = c(iterations, paste(strsplit(filenames[f], split = "_")[[1]][1], j, sep = "_"))
+        
+        # append to master list
+        master_results[[100*k+j]] = mc_results[[j]]
 
-    # add file name to list of iterations
-    iteration = c(iterations, strsplit(filenames[1], split = "_")[[1]][1])
-
-    # add samples in the iteration to the master sample list
-    sample_ids = c(sample_ids, ids)
+        # add samples in the iteration to the master sample list
+        master_samples[[100*k+j]] = as.character(ids[[j]])
     
-    # get genes for each iteration and save to master
-    genes = c(genes, as.character(goi))
+        # get genes for each iteration and save to master
+        master_genes[[100*k+j]] = as.character(goi[[j]])
+    }
 }
 
 # check length 10000
-if(length(master_results) != 2){
-    stop("incorrect number of simulations loaded")
-}
+#if(length(master_results) != 10000){
+#    stop("incorrect number of simulations loaded")
+#}
 
 # get list of GO terms
 uniq_go = master_results[[1]][,1]
@@ -39,10 +48,10 @@ uniq_go = master_results[[1]][,1]
 length(uniq_go)
 
 # make table of iteration and sample IDs
-sample_dict = as.data.frame(matrix(nrow = 413, ncol = 2))
+sample_dict = as.data.frame(matrix(nrow = 413, ncol = 200))
 colnames(sample_dict) = iterations
 for(i in 1:2){
-    sample_dict[,i] = sample_ids[[i]][1]
+    sample_dict[,i] = master_samples[[i]][1]
 }
 sample_dict
 
@@ -50,7 +59,12 @@ sample_dict
 write.table(sample_dict, file = "topGO_10K_sample_dict.txt", quote = FALSE, sep = "\t")
 
 # make table of genes for each iteration
-gene_table = as.data.frame(matrix(nrow = 2, ncol = length(genes[[1]])))
+# maybe replace this with a different approach:
+#   convert each element in the list to a data frame with 1 row
+#   make the column names the gene names
+#   merge onto prior element in the list
+
+gene_table = as.data.frame(matrix(nrow = 2, ncol = length(master_genes[[1]])))
 row.names(gene_table) = iterations
 colnames(gene_table) = genes[[1]]
 
